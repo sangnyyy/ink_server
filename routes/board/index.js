4fccb4d700fd9387f7e2f05cf5e330b691618aeb
chldbtjd2272 <chldbtjd2272@naver.com>
2018-02-26 20:42:39 +0900

    잉크다운

--- routes/board/index.js
+++ routes/board/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 var showbulletin = require('./showbulletin');
 
 router.use('/showbulletin', showbulletin);
+
+
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,49 @@ var express = require('express');
 var router = express.Router();
 var login = require('./login/index');
 var board = require('./board/index');
-
+var schedule = require('node-schedule');
+const mysql = require('mysql');
+const pool = require('../config/dbpool');
+const async = require('async');
 console.log("여기");
 router.use('/login', login);
 router.use('/board', board);
+
+
+
+
+
+var conStyle = '00 00 00 * * 0-7';
+var scheduledJob4 = schedule.scheduleJob(conStyle,
+  function(){
+    let taskArray = [
+    (callback) => {
+      pool.getConnection((err, connection) => {
+        if(err) callback("connection error : " + err, null);
+        else callback(null, connection);
+      });
+    },
+    (connection, callback) => {
+      console.log("여긴가");
+      let checkIdAndPassword = "update bulletin set bulletin_ink = bulletin_ink -1";
+      connection.query(checkIdAndPassword, (err, userData) => {
+        if(err){
+          connection.release();
+          callback("mysql proc error ", null);
+        }else{
+          connection.release();
+          callback(null, "successful rows");
+        }
+      });
+    }
+    ];
+    async.waterfall(taskArray, (err, result) => {
+    if(err) console.log(err);
+    else console.log(result);
+    });
+
+  }
+);
+
 
 module.exports = router;
