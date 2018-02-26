2a2ac85d796e2a32b26a641d1c9ca43f0374a2e8
DESKTOP-M9IB1VS\_miu_minu <minunu93@naver.com>
2018-02-27 01:55:32 +0900

    글목록 뿌려주기, 글하나하나 뿌려주기

--- routes/board/index.js
+++ routes/board/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var showbulletin = require('./showbulletin');
+var showlist = require('./showlist');
 var createbulletin = require('./createbulletin');
 
 router.use('/showbulletin', showbulletin);
+router.use('/showlist', showlist);
 router.use('/createbulletin', createbulletin);
 
 
